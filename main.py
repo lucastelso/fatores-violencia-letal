56bb6f5af8626b9b8463d911c46e979daab5e78e
Lucas <lucastellucciso@gmail.com>
2026-06-04 15:24:48 -0300

    primeiro algoritmo do que vai acontecer no main.py pra ter uma amostra do modelo

---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Orquestra todo o pipeline do projeto
+
+1. Lê os dados locais usados no projeto
+
+2. Printa 5 linhas (aleatorias)
+
+3. Print infos
+
+4. Print estatisticas descritivas
+
+5. Relatorio de VIF 
+    Transforma?
+
+6. Transformação dos componentes
+
+7. Relatório de VIF dos componentes
+
+8. Carregar um modelo pronto?
+
+9. Se não, treinar um modelo?
+    
+            SE SIM --> Hiperparâmetros
+                    
+                   --> Treina o modelo
+            
+            continue
+    
+11. Mostra feature importances 
+
+12. Treina Modelo de Regressão:
+    print(relatório do modelo)
+
+
+"""
