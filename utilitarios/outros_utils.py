7f69d7998727a4cfb4814961c7778d67ba25eb63
Lucas <lucastellucciso@gmail.com>
2026-06-05 10:52:06 -0300

    adicionado um logger para o futuro main.py

--- utilitarios/outros_utils.py
+++ utilitarios/outros_utils.py
@@ -1,6 +1,8 @@
 from typing import Literal
 import pickle
 from pathlib import Path
+import logging
+import sys
 
 
 modelos = Literal[
@@ -21,3 +23,48 @@ def carregar_modelo(
         found_model = pickle.load(file)
     
     return found_model
+
+
+class CustomFormatter(logging.Formatter):
+    """Logger"""
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    cyan = "\x1b[36;20m"
+    reset = "\x1b[0m"
+    
+    # Formato limpo e informativo: Timestamp [NÍVEL] (Arquivo:Linha) -> Mensagem
+    log_format = "%(asctime)s [%(levelname)s] (%(filename)s:%(lineno)d) -> %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + log_format + reset,
+        logging.INFO: cyan + log_format + reset,
+        logging.WARNING: yellow + log_format + reset,
+        logging.ERROR: red + log_format + reset,
+        logging.CRITICAL: bold_red + log_format + reset
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, datefmt="%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
+
+def get_logger(name: str = __name__) -> logging.Logger:
+    """Retorna um logger customizado e configurado para o terminal."""
+    logger = logging.getLogger(name)
+    
+    if not logger.handlers:
+        logger.setLevel(logging.DEBUG)
+
+        # Configura o output para o stdout (terminal)
+        stdout_handler = logging.StreamHandler(sys.stdout)
+        stdout_handler.setLevel(logging.DEBUG)
+        stdout_handler.setFormatter(CustomFormatter())
+        
+        logger.addHandler(stdout_handler)
+        
+    return logger
+
+# Instancia o logger principal
+logger = get_logger("APP")
