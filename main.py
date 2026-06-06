335342f3eaad228c8dd7ca9b99d0770d86940606
Lucas <lucastellucciso@gmail.com>
2026-06-06 12:52:29 -0300

    main.py com simulação do modelo multinível

--- main.py
+++ main.py
@@ -1,55 +1,66 @@
-"""
-
-Orquestra todo o pipeline do projeto
-
-1. Lê os dados locais usados no projeto
-
-2. Printa 5 linhas (aleatorias)
-
-3. Print infos
-
-4. Print estatisticas descritivas
-
-5. Relatorio de VIF 
-    Transforma?
-
-6. Transformação dos componentes
-
-7. Relatório de VIF dos componentes
-
-8. Carregar um modelo pronto?
-
-9. Se não, treinar um modelo?
-    
-            SE SIM --> Hiperparâmetros
-                    
-                   --> Treina o modelo
-            
-            continue
-    
-11. Mostra feature importances 
-
-12. Treina Modelo de Regressão:
-    print(relatório do modelo)
-
-
-"""
 import pandas as pd
 from utilitarios.outros_utils import CustomFormatter
+from models import AvaliadorRegressaoParametrica, DiagnosticadorMultinivel
+from scipy.stats import boxcox
+import statsmodels.api as sm
+
 logger = CustomFormatter.get_logger("PIPELINE")
 
 logger.info('INICIANDO PIPELINE!')
 
+# CARREGANDO OS DADOS
+
 path_dados = r"dados_brutos/fatores_pca.parquet"
-
-fatores = pd.read_parquet(path=path_dados)
-
 logger.info(f"Lendo dados: {path_dados}")
-
-
+fatores = pd.read_parquet(path=path_dados)
 logger.info("Dados encontrados com sucesso!")
-logger.warn("VERIFIQUE A INTEGRIDADE DOS DADOS:")
+
+
+logger.warning("VERIFIQUE A INTEGRIDADE DOS DADOS:")
 
 print(fatores.sample(10))
 
+choice = input(logger.warning('Os dados estão integros?[Y/n]'))
 
+while choice not in ['Y', 'N', 'y', 'n']:
+    choice = input(logger.error("Responda com 'Y' para 'SIM' ou 'n' para 'NAO"))
+
+if choice in ['N', 'n']:
+    raise logger.error('PIPELINE ENCERRADO, DADOS NÃO ESTÃO INTEGROS')
+
+logger.info('Continuando...')
+logger.warning("Fazendo calculo da taxa de violencia em função de indicadores socioeconômicos")
+
+formula_bc = (
+    'TAXA_VIOLENCIA ~ FATOR_POBREZA_DESIGUALDADE + FATOR_INFRAESTRUTURA + '
+    'FATOR_EDUCACAO + FATOR_EMPREGO + FATOR_POPULACAO'
+)
+
+fatores_bc_rlmm = fatores.copy()
+avaliador_param = AvaliadorRegressaoParametrica()
+diag_hlm = DiagnosticadorMultinivel()
+
+fatores_bc_rlmm['TAXA_VIOLENCIA'], lambda_bc = boxcox(fatores_bc_rlmm['TAXA_VIOLENCIA'] + 1)
+
+modelo_bc_2niveis = sm.MixedLM.from_formula(
+    formula=formula_bc,
+    data=fatores_bc_rlmm,
+    groups=fatores["UF"],
+    re_formula='1 + FATOR_EMPREGO'
+)
+
+resultado_bc_2niveis = modelo_bc_2niveis.fit()
+
+
+
+y_predito_bc = resultado_bc_2niveis.fittedvalues.to_numpy()
+residuos_bc = resultado_bc_2niveis.resid.to_numpy()
+print(avaliador_param.diagnostico_quantitativo(
+    residuos_bc, y_predito_bc, 
+    nome_contexto="Modelo Hierárquico (Nível 1)")
+    )
+
+print(f"R² de Nakagawa:{diag_hlm.r2_nakagawa_schielzeth(resultado_bc_2niveis)}")
+print(avaliador_param.plotar_painel_residuos(residuos_bc, y_predito_bc, titulo="Multinível Box-Cox (Nível 1)"))
+print(avaliador_param.plotar_efeitos_aleatorios_multinivel(resultado_bc_2niveis.random_effects))
+print(resultado_bc_2niveis.summary())
